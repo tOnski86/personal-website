5a3f71625262aedb54e57cce925bcc8e993ce90d
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-12-15 12:58:32 +0800

    Adjust Pill sizing

--- src/ui/HeadingGroup.jsx
+++ src/ui/HeadingGroup.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const HeadingGroup = styled.div`
   & > span {
     font-family: monospace;
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     font-weight: 500;
     text-transform: uppercase;
     letter-spacing: 0.1rem;
