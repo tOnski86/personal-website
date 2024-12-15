ecdbd1af1fc293f20dab0893edd989cead8a380b
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-12-16 02:23:19 +0800

    Implement ContactForm layout

--- src/features/contact/ContactForm.jsx
+++ src/features/contact/ContactForm.jsx
@@ -1,17 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const InputRow = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
+import Button from '../../ui/Button';
 
-const InputError = styled.span`
-  font-size: 1.2rem;
-  color: var(--color-secondary-dark);
-`;
-
-const StyledInput = styled.input`
+const sharedInputStyles = css`
   padding: 0.8rem 1.2rem;
   border: none;
   border-radius: 0.2rem;
@@ -27,13 +18,64 @@ const StyledInput = styled.input`
   }
 `;
 
+const InputRow = styled.div`
+  & > * {
+    margin-bottom: 1rem;
+  }
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const InputError = styled.span`
+  font-size: 1.2rem;
+  color: var(--color-secondary-dark);
+`;
+
+const StyledInput = styled.input`
+  ${sharedInputStyles}
+`;
+
+const StyledTextArea = styled.textarea`
+  ${sharedInputStyles}
+  resize: none;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
 function ContactForm() {
   return (
-    <InputRow>
-      <label htmlFor='name'>Your Name</label>
-      <StyledInput type='text' id='name' required />
-      <InputError>This field is required</InputError>
-    </InputRow>
+    <>
+      <InputRow>
+        <InputGroup>
+          <label htmlFor='name'>Your Name</label>
+          <StyledInput type='text' id='name' required />
+          <InputError>This field is required</InputError>
+        </InputGroup>
+
+        <InputGroup>
+          <label htmlFor='email'>Email Address</label>
+          <StyledInput type='email' id='email' required />
+          <InputError>This field is required</InputError>
+        </InputGroup>
+
+        <InputGroup>
+          <label htmlFor='message'>How can I help?</label>
+          <StyledTextArea type='text' id='message' required rows={5} />
+          <InputError>This field is required</InputError>
+        </InputGroup>
+
+        <ButtonContainer>
+          <Button variant='primary solid'>Submit</Button>
+        </ButtonContainer>
+      </InputRow>
+    </>
   );
 }
 
